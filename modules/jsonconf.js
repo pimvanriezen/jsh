657c72d86c36bd9ebd7c16f6b399c02e5a779b90
Pim van Riezen <pi@midilab.nl>
2019-12-18 09:32:41 +0100

    simplify jsonconf

--- modules/jsonconf.js
+++ modules/jsonconf.js
@@ -1,21 +1,4 @@
 var subProxy = {};
-subProxy.has = function (target, key) {
-    return target.data.hasOwnProperty (key);
-}
-
-subProxy.get = function (target, key) {
-    if (typeof (target.data[key] == "object")) {
-        return new Proxy({data:target.data[key],parent:target,id:key},subProxy);
-    }
-    return target.data[key];
-}
-
-subProxy.set = function (target, key, value) {
-    console.log ("set "+key+" = "+value);
-    target.data[key] = value;
-    target.parent[target.id] = target.data;
-}
-
 var cProxy = {};
 cProxy.has = function (target, key) {
     if (! target.loaded) {
@@ -29,9 +12,6 @@ cProxy.get = function (target, key) {
     if (! target.loaded) {
         target.data = load (target.fileName);
         target.loaded = true;
-    }
-    if (typeof (target.data[key]) == "object") {
-        return new Proxy({data:target.data[key],parent:target,id:key},subProxy);
     }
     return target.data[key];
 }
