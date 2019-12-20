2394a4fa9eb2ed4815661fe820c2b24e779a5800
Pim van Riezen <pi@midilab.nl>
2019-12-20 18:51:37 +0100

    jsonconf: autoreload, only save when requested.

--- modules/jsonconf.js
+++ modules/jsonconf.js
@@ -1,5 +1,15 @@
+var now = function() { return new Date; }
 var cProxy = {};
+cProxy.needReload = function (target) {
+    if (now() - target.lastModified > 1000) {
+        if (stat(target.fileName).mtime > target.lastModified) {
+            return true;
+        }
+    }
+    return false;
+}
 cProxy.has = function (target, key) {
+    if (target.loaded) target.loaded = !cProxy.needReload(target);
     if (! target.loaded) {
         target.data = sys.read (target.fileName);
         target.loaded = true;
@@ -8,6 +18,7 @@ cProxy.has = function (target, key) {
 }
 
 cProxy.get = function (target, key) {
+    if (target.loaded) target.loaded = !cProxy.needReload(target);
     if (! target.loaded) {
         target.data = load (target.fileName);
         target.loaded = true;
@@ -16,14 +27,26 @@ cProxy.get = function (target, key) {
 }
 
 cProxy.set = function (target, key, value) {
+    if (target.loaded) target.loaded = !cProxy.needReload(target);
     if (! target.loaded) {
         target.data = load (target.fileName);
         target.loaded = true;
     }
     target.data[key] = value;
-    save (target.data, target.fileName);
+    if (target.autosave) save (target.data, target.fileName);
 }
 
-module.exports = function(fname) {
-    return new Proxy ({loaded:false,fileName:fname,data:{}}, cProxy);
+module.exports = function(fname,doautosave) {
+    var st = stat(fname);
+    if (! st) {
+        return null;
+    }
+    
+    return new Proxy ({
+        autosave:(doautosave?true:false),
+        loaded:false,
+        fileName:fname,
+        lastModified:st.mtime,
+        data:{}
+    }, cProxy);
 }
