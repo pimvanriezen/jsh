da6c164c8ee1629c88ea29851894198e08f25ec7
Pim van Riezen <pi@inimini.office.newvm.com>
2019-12-23 12:06:48 +0100

    implement dd

--- modules/global.d/unixuserland.js
+++ modules/global.d/unixuserland.js
@@ -429,6 +429,57 @@ if (which ("make")) {
     ]);
 }
 
+dd = setapi ([
+    {name:"dd"},
+    {
+        opt:{"inputFile":[]},
+        helptext:"Load input data from file. If omitted, a steam of NUL "+
+                 "characters is used."
+    },
+    {opt:{"outputFile":[]},helptext:"Output file to write to"},
+    {opt:{"count":[]},helptext:"Number of blocks to copy"},
+    {opt:{"blockSize":[]},helptext:"Read/write block size in bytes"},
+    {
+        opt:{"inputOffset":[]},
+        helptext:"Offset in blocks to start reading from"
+    },
+    {
+        opt:{"outputOffset":[]},
+        helptext:"Offset in blocks to seek forward on destination prior "+
+                 "to writing"
+    },
+    {flag:{"noTruncate":""},helptext:"Don't truncate output file"},
+    {flag:{"sparse":""},helptext:"Write output sparsely, skipping empty blocks"},
+    {helptext:<<<
+        Copies binary data between two files/devices.
+    >>>},
+    {f:function(args) {
+        argv = [];
+        if (args.inputFile) argv.push ("if="+args.inputFile);
+        else {
+            if (! args.count) {
+                printerr ("Cannot write zero without a block count");
+                return false;
+            }
+            argv.push ("if=/dev/zero");
+        }
+        if (args.outputFile) argv.push ("of="+args.outputFile);
+        else {
+            printerr ("No output file specified");
+            return false;
+        }
+        if (args.count) argv.push ("count="+parseInt(args.count));
+        if (args.blockSize) argv.push ("bs="+parseInt(args.blockSize));
+        else argv.push ("bs=1024");
+        if (args.inputOffset) argv.push ("skip="+parseInt(args.inputOffset));
+        if (args.outputOffset) argv.push ("seek="+parseInt(args.outputOffset));
+        if (args.noTruncate) argv.push ("notrunc");
+        if (args.sparse) argv.push ("sparse");
+        if (! sys.runconsole ("dd", argv)) return false;
+        return;
+    }}
+]);
+
 // ----------------------------------------------------------------------------
 stty = setapi ([
     {name:"stty"},
