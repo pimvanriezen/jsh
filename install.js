ad73bd532955b30c242871d7ac12ff361a2fa7cd
Pim van Riezen <pi@midilab.nl>
2019-12-25 01:59:36 +0100

    tweak

--- install.js
+++ install.js
@@ -9,32 +9,48 @@ if (argv.length>1) {
     cp (a,b);
 }*/
 
-var mycp = function(src,dst) {
-    var srcid = src.replace(/.*\//,"");
-    console.log (srcid.padEnd(20), " -> ", dst);
+var mycp = function(src,dst,srcid) {
+    if (! srcid) srcid = src.replace(/.*\//,"");
+    echo (srcid.padEnd(20), " -> ", dst);
     cp (src, dst);
+}
+
+var mymkdir = function(name,mode) {
+    echo ("".padEnd(20), " MK ", name);
+    mkdir (name, mode);
+}
+
+var banner = function(txt) {
+    echo (("---[ "+txt+" ]").padEnd(sys.winsize(),"-"));
 }
 
 var f = function(n) { return basedir + '/' + n; }
 
-if (! exists (f("etc/jsh"))) mkdir ("etc/jsh");
-if (! exists (f("etc/jsh/modules"))) mkdir ("etc/jsh/modules");
-if (! exists (f("etc/jsh/modules/global.d"))) mkdir ("etc/jsh/modules/global.d");
-if (! exists (f("etc/jsh/modules/app"))) mkdir ("etc/jsh/modules/app");
+banner ("Creating directories");
+if (! exists (f("etc/jsh"))) mymkdir ("etc/jsh");
+if (! exists (f("etc/jsh/modules"))) mymkdir ("etc/jsh/modules");
+if (! exists (f("etc/jsh/modules/global.d"))) mymkdir ("etc/jsh/modules/global.d");
+if (! exists (f("etc/jsh/modules/app"))) mymkdir ("etc/jsh/modules/app");
 
+banner ("Copying base files");
 mycp ("jshrc",f("etc/jsh/jshrc"));
 mycp ("bin/jsh",f("bin/jsh"));
+
+banner("Copying base modules");
 $("modules/*").each (function (file) {
     if (! file.isDir) {
         mycp (file, f("etc/jsh/"+file));
     }
 });
+
+banner ("Copying global includes");
 $("modules/global.d/*.js").each (function (file) {
     mycp (file, f("etc/jsh/"+file));
 });
 
+banner ("Copying apps");
 $("modules/app/*").each (function (dir) {
-    mkdir (f("etc/jsh/modules/app/"+dir));
-    mycp (dir + "/app.js", f("etc/jsh/"+dir+"/app.js"));
+    mkdir (f("etc/jsh"+dir));
+    mycp (dir + "/app.js", f("etc/jsh/"+dir+"/app.js"),dir);
 });
 
