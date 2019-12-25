43ee5df9aafa0a1d6de3bafcc497f64ab60e06c1
Pim van Riezen <pi@midilab.nl>
2019-12-25 12:22:38 +0100

    Make cd history accessible through cd.history(). Make cd() with no arguments jump to homedir

--- modules/app/cd/app.js
+++ modules/app/cd/app.js
@@ -1,34 +1,51 @@
+var cdhistory = [];
+
 var cd = function(arg) {
-    if (! cd.history) cd.history = [];
+    if (! cdhistory) cdhistory = [];
     var newdir = arg;
     if (typeof (arg) == 'number') {
-        if (arg < 0 && cd.history.length + arg >= 0) {
-            newdir = cd.history[cd.history.length+arg];
+        if (arg < 0 && cdhistory.length + arg >= 0) {
+            newdir = cdhistory[cdhistory.length+arg];
         }
         else return false;
     }
-    cd.history.push(cwd());
-    while (cd.history.length > 32) cd.history.slice (0,1);
+    else if (arg === undefined) newdir = userdb[sys.getuid()].home;
+    cdhistory.push(cwd());
+    while (cdhistory.length > 32) cdhistory.slice (0,1);
     return sys.cd (newdir);
 }
-
-cd.history = [];
 
 cd.help = function() {
     setapi.helptext ({
         name:"cd",
         args:[
-            {name:"path",text:"Relative or absolute path of the new "+
-                              "directory, "+
-                              "or a negative number indicating a relative "+
-                              "position in the history."}
+            {name:"path",text: <<<
+                Relative or absolute path of the new directory, or a
+                negative number indicating a relative position in
+                the history. If left empty, the path is assumed
+                to be the home directory of the current user.
+            >>>}
         ],
         text:<<<
             Pushes the current working directory into the history, and changes
-            it to a new directory.
+            it to a new directory. Call cd.history() to get a view of the
+            current history.
         >>>
     });
 }
 
-module.version = "1.0.1";
+cd.history = function() {
+    return cdhistory;
+}
+
+cd.history.help = function() {
+    setapi.helptext ({
+        name:"cd.history",
+        text:<<<
+            Displays the history of working directories.
+        >>>
+    });
+}
+
+module.version = "1.0.2";
 module.exports = cd;
