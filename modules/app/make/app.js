a290f8b18bb0af6a465c2a1d1e409531e8757070
Pim van Riezen <pi@midilab.nl>
2019-12-25 12:51:29 +0100

    add make app

---
+++ modules/app/make/app.js
@@ -0,0 +1,14 @@
+if (exists ("make")) {
+    var make = setapi ([
+        {name:"make"},
+        {literal:"make"},
+        {opt:{"file":"-f"},helptext:"Use file instead of Makefile"},
+        {opt:{"target":[]},helptext:"Build target"},
+        {console:true},
+        {helptext:"Runs the make build utility."}
+    ]);
+
+    module.version = "1.0.0";
+    module.exports = make;
+}
+
