21f81938559ebdfb8b10c885ff03d58bed4a14f0
Pim van Riezen <pi@midilab.nl>
2019-12-26 14:04:51 +0100

    Add len.js

---
+++ modules/len.js
@@ -0,0 +1,10 @@
+var lnfunc = function(obj) {
+    if (! obj) return 0;
+    if (typeof(obj) == "string") return obj.length;
+    if (typeof(obj) == "object") {
+        if (obj.length) return obj.length;
+    }
+    return (""+obj).length;
+}
+
+module.exports = lnfunc;
