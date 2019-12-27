ae59048f9407160f9b32de22030ff0924426442b
Pim van Riezen <pi@midilab.nl>
2019-12-27 22:06:11 +0100

    Move api help for object prototypes to their own global.d's

--- modules/global.d/sys.array.js
+++ modules/global.d/sys.array.js
@@ -1,7 +1,50 @@
+// ============================================================================
+// Extensions to the Array class
+// ============================================================================
+Array.help = function() {
+    print (setapi.textformat (<<<
+        The following extra functions have been added to the Array
+        class prototype. Type help(Array.function) for more specific
+        information.
+        
+        Available functions on constructed objects:
+    >>>));
+    
+    var list = [];
+    for (i in Array) {
+        if (Array[i].help) list.push("Array."+i);
+    }
+    
+    print (new AutoColumn().setData(list).indent(4).format());
+}
+
+setapi (Array, "Array");
+
+// ============================================================================
+// Array::contains
+// ============================================================================
 Object.defineProperty (Array.prototype, 'contains', {
     value: function(id) { return this.indexOf(id)>=0; }
 });
 
+Array.contains = {help:function() {
+    setapi.helptext ({
+        name:"array.contains",
+        args:[
+            {name:"value",text:"The value to look for"}
+        ],
+        text:<<<
+            Returns true if the provided value is contained within
+            the array.
+        >>>
+    });
+}}
+
+setapi (Array.contains, "Array.contains");
+
+// ============================================================================
+// Array::remove
+// ============================================================================
 Object.defineProperty (Array.prototype, 'remove', {
     value: function(match) {
         if (typeof(match) == "string") {
@@ -17,6 +60,26 @@ Object.defineProperty (Array.prototype, 'remove', {
     }
 });
 
+Array.remove = {help:function() {
+    setapi.helptext ({
+        name:"array.remove",
+        args:[
+            {name:"match",text:<<<
+                Either a string that should be removed from the array,
+                or an index.
+            >>>}
+        ],
+        text:<<<
+            Removes an element from the array, modifying it.
+        >>>
+    });
+}}
+
+setapi (Array.remove, "Array.remove");
+
+// ============================================================================
+// Array::sum
+// ============================================================================
 Object.defineProperty (Array.prototype, 'sum', {
     value: function() {
         var res = 0;
@@ -25,6 +88,21 @@ Object.defineProperty (Array.prototype, 'sum', {
     }
 });
 
+Array.sum = {help:function() {
+    setapi.helptext ({
+        name:"array.sum",
+        text:<<<
+            Returns the sum of all elements in the array, provided
+            they can be parsed as numbers. Will return NaN if not.
+        >>>
+    });
+}}
+
+setapi (Array.sum, "Array.sum");
+
+// ============================================================================
+// Array::cut
+// ============================================================================
 Object.defineProperty (Array.prototype, 'cut', {
     value: function(field,sep) {
         var res = [];
@@ -37,6 +115,34 @@ Object.defineProperty (Array.prototype, 'cut', {
     }
 });
 
+Array.cut = {help:function() {
+    setapi.helptext ({
+        name:"array.cut",
+        args:[
+            {name:"index",text:<<<
+                The field to cut out, indexing starts at 0. If the field
+                provided is negative, it is counted from the right, with
+                -1 being the last field.
+            >>>},
+            {name:"delimiter",text:<<<
+                Character or regular expression pattern to use as a means
+                of splitting up the string. If this argument is left out,
+                it defaults to splitting on arbitrary amounts of white
+                space.
+            >>>}
+        ],
+        text:<<<
+            Cuts a specific field out of each string in the array. Returns
+            an array with the transformation applied.
+        >>>
+    });
+}}
+
+setapi (Array.cut,"Array.cut");
+
+// ============================================================================
+// Array::grep
+// ============================================================================
 Object.defineProperty (Array.prototype, 'grep', {
     value: function(re,srch,repl) {
         if (typeof (re) == "string") re = new RegExp (re);
@@ -53,3 +159,29 @@ Object.defineProperty (Array.prototype, 'grep', {
         return res;
     }
 });
+
+Array.grep = {help:function() {
+    setapi.helptext ({
+        name:"array.grep",
+        args:[
+            {name:"pattern",text:<<<
+                Regular expression pattern to match lines against.
+            >>>},
+            {name:"process",text:<<<
+                [Optional] Regular expression pattern that will
+                be applied to a matchine line, and either deleted
+                or replaced with the third argument.
+            >>>},
+            {name:"replace",text:<<<
+                Replacement string for processing
+            >>>}
+        ],
+        text:<<<
+            Given the array contains a list of strings, a new array
+            is returned with only the strings matching the pattern,
+            with transformation optionally applied.
+        >>>
+    })
+}}
+
+setapi (Array.grep,"Array.grep");
--- modules/global.d/sys.string.js
+++ modules/global.d/sys.string.js
@@ -1,6 +1,28 @@
 // ============================================================================
 // Augmentations for the string class
 // ============================================================================
+String.help = function() {
+    print (setapi.textformat (<<<
+        The following extra functions have been added to the String
+        class prototype. Type help(String.function) for a more
+        specific list.
+        
+        Available functions on constructed objects:
+    >>>));
+    
+    var list = [];
+    for (i in String) {
+        if (String[i].help) list.push("String."+i);
+    }
+    
+    print (new AutoColumn().setData(list).indent(4).format());
+}
+
+setapi (String, "String");
+
+// ============================================================================
+// String::padStart
+// ============================================================================
 String.prototype.padStart = function(len,c) {
     if (c === undefined) c = " ";
     var res = this.slice(0,len);
@@ -9,6 +31,27 @@ String.prototype.padStart = function(len,c) {
     return res;
 }
 
+String.padStart = {help:function() {
+    setapi.helptext ({
+        name:"string.padStart",
+        args:[
+            {name:"length",text:"Length to pad the string to"},
+            {name:"char",text:"[Optional] Character to use for padding"},
+        ],
+        text:<<<
+            Cuts the string to an exact length. If the string is shorter
+            than the length provided, it is padded from the left with
+            spaces (or the provided character). The newly built String
+            is returned, and the original is left untouched.
+        >>>
+    });
+}}
+
+setapi (String.padStart, "String.padStart");
+
+// ============================================================================
+// String::padEnd
+// ============================================================================
 String.prototype.padEnd = function(len,c) {
     if (c === undefined) c = " ";
     var res = this.slice(0,len);
@@ -17,10 +60,48 @@ String.prototype.padEnd = function(len,c) {
     return res;
 }
 
+String.padEnd = {help:function() {
+    setapi.helptext ({
+        name:"string.padEnd",
+        args:[
+            {name:"length",text:"Length to pad the string to"},
+            {name:"char",text:"[Optional] Character to use for padding"},
+        ],
+        text:<<<
+            Cuts the string to an exact length. If the string is shorter
+            than the length provided, it is padded at the right side with
+            spaces (or the provided character). The newly built String
+            is returned, and the original is left untouched.
+        >>>
+    });
+}}
+
+setapi (String.padEnd, "String.padEnd");
+
+// ============================================================================
+// String::save
+// ============================================================================
 String.prototype.save = function(path) {
     sys.write (""+this, ""+path);
 }
 
+String.save = {help:function() {
+    setapi.helptext ({
+        name:"string.save",
+        args:[
+            {name:"path",text:"Path and filename to save to."}
+        ],
+        text:<<<
+            Writes a string to disk.
+        >>>
+    })
+}}
+
+setapi (String.save, "String.save");
+
+// ============================================================================
+// String::summarize
+// ============================================================================
 String.prototype.summarize = function(sz) {
     if (this.length <= sz) return this.toString();
     if (sz < 11) return this.toString();
@@ -33,10 +114,58 @@ String.prototype.summarize = function(sz) {
     return res;
 }
 
+String.summarize = {help:function() {
+    setapi.helptext ({
+        name:"string.summarize",
+        args:[
+            {name:"width",text:"Maximum width of the string."}
+        ],
+        text:<<<
+            Shortens a string if it exceeds a certain width, taking
+            even ends from the left and the right, with an ellipsis
+            in the middle representing the excess characters.
+        >>>
+    })
+}}
+
+setapi (String.summarize, "String.summarize");
+
+// ============================================================================
+// String::grep
+// ============================================================================
 String.prototype.grep = function(re,srch,repl) {
     return this.split('\n').grep (re,srch,repl);
 }
 
+String.grep = {help:function() {
+    setapi.helptext ({
+        name:"string.grep",
+        args:[
+            {name:"pattern",text:<<<
+                Regular expression pattern to match lines against.
+            >>>},
+            {name:"process",text:<<<
+                [Optional] Regular expression pattern that will
+                be applied to a matchine line, and either deleted
+                or replaced with the third argument.
+            >>>},
+            {name:"replace",text:<<<
+                Replacement string for processing
+            >>>}
+        ],
+        text:<<<
+            Splits a string up by newlines, then pulls them through
+            Array.grep() to yield an array of processed lines
+            matching the given pattern.
+        >>>
+    })
+}}
+
+setapi (String.grep, "String.grep");
+
+// ============================================================================
+// String::wrap
+// ============================================================================
 String.prototype.wrap = function (cols) {
     var str = ""+this;
     var words = str.split (' ');
@@ -55,6 +184,24 @@ String.prototype.wrap = function (cols) {
     return res.join('\n');
 }
 
+String.wrap = {help:function() {
+    setapi.helptext ({
+        name:"string.wrap",
+        args:[
+            {name:"width",text:"Maximum width of text lines."}
+        ],
+        text:<<<
+            Returns a word-wrapped version of the string, with newlines
+            added for every time a line would exceed the width.
+        >>>
+    })
+}}
+
+setapi (String.wrap, "String.wrap");
+
+// ============================================================================
+// String::cut
+// ============================================================================
 String.prototype.cut = function (field, sep) {
     if (! sep) sep = /[ \t]+/;
     var splt = this.split (sep);
@@ -64,6 +211,34 @@ String.prototype.cut = function (field, sep) {
     return splt[field];
 }
 
+String.cut = {help:function() {
+    setapi.helptext ({
+        name:"string.cut",
+        args:[
+            {name:"index",text:<<<
+                The field to cut out, indexing starts at 0. If the field
+                provided is negative, it is counted from the right, with
+                -1 being the last field.
+            >>>},
+            {name:"delimiter",text:<<<
+                Character or regular expression pattern to use as a means
+                of splitting up the string. If this argument is left out,
+                it defaults to splitting on arbitrary amounts of white
+                space.
+            >>>}
+        ],
+        text:<<<
+            Cuts a specific field out of a string that can reasonably
+            be split up by some kind of delimiter.
+        >>>
+    });
+}}
+
+setapi (String.cut, "String.cut");
+
+// ============================================================================
+// String::rewrap
+// ============================================================================
 String.prototype.rewrap = function (cols) {
     var res = "";
     var str = ""+this;
@@ -80,6 +255,26 @@ String.prototype.rewrap = function (cols) {
     return res;   
 }
 
+String.rewrap = {help:function() {
+    setapi.helptext ({
+        name:"string.rewrap",
+        args:[
+            {name:"width",text:"New maximum line width"}
+        ],
+        text:<<<
+            Transforms text that has potentially already been wrapped,
+            splits it up into paragraphs, and word-wraps each paragraph
+            to the desired line length. Paragraphs should have an empty
+            line between them for this to work.
+        >>>
+    })
+}}
+
+setapi (String.rewrap, "String.rewrap");
+
+// ============================================================================
+// String::colorMatch
+// ============================================================================
 String.prototype.colorMatch = function (expr, code) {
     var ansi = code;
     var re = expr;
