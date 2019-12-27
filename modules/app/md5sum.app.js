89ae8706c3cf92c4372611617531f7ec2525c517
Pim van Riezen <pi@midilab.nl>
2019-12-27 12:10:04 +0100

    debug

--- modules/app/md5sum.app.js
+++ modules/app/md5sum.app.js
@@ -7,6 +7,9 @@ var md5sum = setapi ([
     {literal:md5cmd},
     {arg:"path",helptext:"File to checksum"},
     {process:function(dat) {
+        if (dat === true) {
+            throw new Error ("md5sum did jack on "+path);
+        }
         dat = dat.replace('\n','');
         if (md5cmd == "md5sum") return dat.split(' ')[0];
         return dat.replace (/^.* /,"");
